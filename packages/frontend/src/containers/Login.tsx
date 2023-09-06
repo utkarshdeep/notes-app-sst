9d039ff2cc082e51f194ad30901dda01941dcf12
Utkarsh Deep <utkarshd80@gmail.com>
2023-09-07 00:08:02 +0530

    App development complete

--- packages/frontend/src/containers/Login.tsx
+++ packages/frontend/src/containers/Login.tsx
@@ -5,13 +5,10 @@ import LoaderButton from "../components/LoaderButton.tsx";
 import "./Login.css";
 import { Auth } from "aws-amplify";
 import { useAppContext } from "../lib/contextLib";
-import { useNavigate } from "react-router-dom";
 import { onError } from "../lib/errorLib";
 import { useFormFields } from "../lib/hooksLib";
 
 export default function Login() {
-
-    const nav = useNavigate();
 
     const { userHasAuthenticated } = useAppContext();
 
@@ -32,7 +29,6 @@ export default function Login() {
         try {
             await Auth.signIn(fields.email, fields.password);
             userHasAuthenticated(true);
-            nav("/");
         } catch (error) {
             onError(error);
             setIsLoading(false);
